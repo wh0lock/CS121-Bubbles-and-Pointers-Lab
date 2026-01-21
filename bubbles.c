63ad999283c669a80145f9335c57f02639b768e4
Emily <165333619+wh0lock@users.noreply.github.com>
2026-01-21 18:11:46 +0000

    Starter code

---
+++ bubbles.c
@@ -0,0 +1,29 @@
+// bubbles and pointers
+// gives array of numbers and sorts them with bubble sort algorithm
+// Emily Adams
+
+#include <stdio.h>
+const int MAX = 9;
+
+void printValues(int*);
+void sort(int*);
+void swap(int*, int*);
+
+int main(){
+	int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+	printf("Before: \n");
+	printValues(values);
+
+	// test swap
+	int x = 3;
+	int y = 5;
+	printf("x: %d, y: %d \n", x, y);
+	swap(&x, &y);
+	printf("x: %d, y: %d \n", x, y);
+
+	sort(values);
+	printf("After: \n");
+	printValues(values);
+
+	return(0);
+} // end main
